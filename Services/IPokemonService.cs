4782e769393de7e2eb074e5d7b289caa41737175
Rodrigo Romano <rodrigo@rodrigoromano.net>
2017-12-23 21:14:56 +0100

    refactor

--- Services/IPokemonService.cs
+++ Services/IPokemonService.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-namespace pokelist.Model
+using pokelist.Model;
+
+namespace pokelist.Services
 {
     public interface IPokemonService
     {
